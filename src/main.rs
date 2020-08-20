e92bb50fb214f036cc39efef111a818a55ed6a74
Sam Anklesaria <sam@spacemaker.ai>
2020-08-20 19:08:12 -0400

    Refactor thread handling

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,28 @@
 extern crate gdk;
 extern crate gtk;
 extern crate gio;
-#[macro_use]
 extern crate glib;
 extern crate sourceview;
 extern crate sha2;
 extern crate bimap;
 extern crate bincode;
 extern crate serde;
-extern crate once_cell;
 
 use gtk::prelude::*;
 use gio::prelude::*;
 use gtk::*;
 use sourceview::{LanguageManagerExt, ViewExt, BufferExt};
 use glib::GString;
-use glib::subclass;
-use glib::translate::*;
-use gtk::subclass::prelude::*;
 use pyo3::prelude::*;
 use pyo3::exceptions;
 use pyo3::conversion::AsPyPointer;
 use pyo3::ffi;
 use sha2::{Sha256, Digest};
-use sha2::digest::FixedOutput;
-use sha2::digest::generic_array::GenericArray;
 use bimap::BiMap;
 
 use serde::{Serialize};
-use std::cell;
 use std::thread;
 use std::time::Duration;
-use std::sync::mpsc;
 use std::sync::{Mutex, Arc};
 use std::sync::atomic::{Ordering, AtomicBool};
 use std::collections::HashMap;
@@ -57,8 +48,7 @@ fn build_branch_toggle(vbox: &Box) {
     r2.join_group(Some(&r1));
     hbox.pack_start(&r1, false, false, 10);
     hbox.pack_start(&r2, false, false, 10);
-    vbox.pack_start(&hbox, false, false, 2);
-}
+    vbox.pack_start(&hbox, false, false, 2);}
 
 fn new_history_header(lenf: f64) -> Box {
     let hbox = Box::new(Orientation::Horizontal, 0);
@@ -67,20 +57,15 @@ fn new_history_header(lenf: f64) -> Box {
         let old_val = a.get_value();
         let new_val = old_val.round();
         if old_val != new_val {
-            a.set_value(new_val);
-        }
-    });
+            a.set_value(new_val); }});
     let scale = Scale::new(Orientation::Horizontal, Some(&adj));
     scale.set_draw_value(false);
     hbox.pack_start(&scale, true, true, 0);
     let btn = Button::with_label("Branch");
     hbox.pack_end(&btn, false, false, 2);
     hbox.set_size_request(500, -1);
-    unsafe {
-        hbox.set_data("adj", adj);
-    }
-    hbox
-}
+    unsafe {hbox.set_data("adj", adj);}
+    hbox}
 
 fn add_history_header(tv: &sourceview::View, nm: ParsedName) {
     let lenf = nm.len as f64;
@@ -92,9 +77,7 @@ fn add_history_header(tv: &sourceview::View, nm: ParsedName) {
             unsafe {
                 let adj : &Adjustment = b.get_data("adj").unwrap();
                 adj.set_upper(lenf);
-                adj.set_value(lenf);
-            }
-        }
+                adj.set_value(lenf); }}
         None => {
             eprintln!("NO CHILD FOUND");
             let mut iter = tb.get_iter_at_line(nm.line);
@@ -110,10 +93,7 @@ fn add_history_header(tv: &sourceview::View, nm: ParsedName) {
             tb.apply_tag(&tag, &tb.get_iter_at_line(0), &iter);
             let b = new_history_header(lenf);
             tv.add_child_at_anchor(&b, &anchor);
-            b.show_all();
-        }
-    }
-}
+            b.show_all(); }}}
 
 type Hash = [u8; 32];
 
@@ -143,29 +123,25 @@ struct PyState {
     // and controls when we display ASTs using symbols instead of inlining
     names: BiMap<String, Hash>,
 
-    // Allows propagating a new definition to all exprs that used the old one
-    dependents: HashMap<Hash, Vec<Hash>>,
-
-    // What is the previous definition historically?
-    prev: HashMap<Hash, Hash>,
-
     // How long is the chain of hashes ending at this hash?
     // Not all hashes have entries- this is just a cache for branch tips
     len: HashMap<Hash, u32>,
 
-    // Maps from name to branch to tip
-    branches: HashMap<String, HashMap<String, Hash>>,
-
-    // Allows us to replace something we previously assumed was a primitive
-    // with a user defined hash
-    prim_dependents: HashMap<String, Vec<Hash>>
 }
+
+impl PyState {
+    fn new() -> PyState {
+        PyState {
+            defs: HashMap::new(),
+            names: BiMap::new(),
+            len: HashMap::new()}}
+}
+
 
 fn hash_expr(st: &mut PyState, expr: PyExpr) -> Hash {
     let hash = Sha256::digest(&bincode::serialize(&expr).unwrap()).into();
     st.defs.insert(hash, expr);
-    hash
-}
+    hash}
 
 fn hash_val(ptrs: PyPtrs, st: &mut PyState, val: &PyAny, args: &Vec<String>) -> PyResult<Hash> {
     let ty : *const ffi::PyObject = val.get_type().as_ptr();
@@ -181,9 +157,7 @@ fn hash_val(ptrs: PyPtrs, st: &mut PyState, val: &PyAny, args: &Vec<String>) -> 
             Some(ix) => Ok(Sha256::digest(&bincode::serialize(&PyExpr::DeBruijn(ix)).unwrap()).into()),
             None => match st.names.get_by_left(&name) {
                 Some(&h) => Ok(h),
-                None => Ok(hash_expr(st, PyExpr::Prim(name)))
-            }
-        }
+                None => Ok(hash_expr(st, PyExpr::Prim(name)))}}
     } else if ty == ptrs.bin_ty {
         let left = hash_val(ptrs, st, val.getattr("left")?, args)?;
         let right = hash_val(ptrs, st, val.getattr("right")?, args)?;
@@ -196,24 +170,18 @@ fn hash_val(ptrs: PyPtrs, st: &mut PyState, val: &PyAny, args: &Vec<String>) -> 
         Ok(hash_expr(st, expr))
     } else {
         let msg = format!("Unknown ast node with type {}", val.get_type().repr()?);
-        exceptions::TypeError::into(msg)
-    }
-}
+        exceptions::TypeError::into(msg)}}
 
 fn update_name(st: &mut PyState, name: String, val: Hash, prev: Option<Hash>) -> u32 {
     st.names.insert(name, val);
     match prev {
         None => {
             st.len.insert(val, 1);
-            1
-        }
+            1}
         Some(h) => {
             let len = st.len.get(&h).unwrap() + 1;
             st.len.insert(val, len);
-            len
-        }
-    }
-}
+            len}}}
 
 fn add_def(st: &mut PyState, parsed_names: &mut Vec<ParsedName>, name: String, hash: Hash, line: i32) {
     match st.names.get_by_left(&name) {
@@ -222,11 +190,7 @@ fn add_def(st: &mut PyState, parsed_names: &mut Vec<ParsedName>, name: String, h
             parsed_names.push(ParsedName{
                 line: line,
                 hash: hash,
-                len: update_name(st, name, hash, Some(h))
-            })
-        }
-    }
-}
+                len: update_name(st, name, hash, Some(h))})}}}
 
 fn hash_def(ptrs: PyPtrs, st: &mut PyState, def: &PyAny, offset: i32) -> PyResult<Vec<ParsedName>> {
     let args = def.getattr("args")?.getattr("args")?.iter()?.map(|x| {
@@ -249,15 +213,13 @@ fn hash_def(ptrs: PyPtrs, st: &mut PyState, def: &PyAny, offset: i32) -> PyResul
             return Ok(parsed_names);
         }
     }
-    exceptions::TypeError::into("No return statement")
-}
+    exceptions::TypeError::into("No return statement")}
 
 fn py_parse<'a>(ptrs: PyPtrs, st: &mut PyState, ast: &'a PyModule, code: &str, offset: i32) ->
         PyResult<Vec<Vec<ParsedName>>> {
     ast.call1("parse", (code,))?.getattr("body")?.iter()?.map(|x_r| {
         hash_def(ptrs, st, x_r?, offset)
-    }).collect()
-}
+    }).collect()}
 
 // To check which type of AST nodes we parsed, we keep a pointer to each node type
 #[derive(Copy, Clone)]
@@ -270,45 +232,61 @@ struct PyPtrs {
     const_ty: *const ffi::PyObject
 }
 
+impl PyPtrs {
+    fn new(ast: &PyModule) -> PyPtrs {
+        PyPtrs{
+            ret_ty: (ast.getattr("Return").unwrap()).as_ptr(),
+            asn_ty: (ast.getattr("Assign").unwrap()).as_ptr(),
+            bin_ty: (ast.getattr("BinOp").unwrap()).as_ptr(),
+            call_ty: (ast.getattr("Call").unwrap()).as_ptr(),
+            name_ty: (ast.getattr("Name").unwrap()).as_ptr(),
+            const_ty: (ast.getattr("Constant").unwrap()).as_ptr()}}
+}
+
+
 struct Work {
     do_work: AtomicBool,
     die: AtomicBool,
     content: Mutex<Option<(GString,i32)>>
 }
 
-fn spawn_parser(receiver: mpsc::Receiver<(GString, i32)>, sender: glib::Sender<ParsedName>) {
-    let work = Arc::new(Work{
-        do_work: AtomicBool::new(false),
-        die: AtomicBool::new(false),
-        content: Mutex::new(None)
-    });
+impl Work {
+    fn new() -> Work {
+        Work{
+            do_work: AtomicBool::new(false),
+            die: AtomicBool::new(false),
+            content: Mutex::new(None) }}
+}
+
+fn setup_textview(tv: &sourceview::View) {
+    tv.set_auto_indent(true);
+    tv.set_indent_on_tab(true);
+    tv.set_smart_backspace(true);
+    tv.set_indent_width(2)}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::new(app);
+    window.set_title("Sting");
+    window.set_default_size(350, 70);
+    let lm = sourceview::LanguageManager::get_default().unwrap();
+    let python = lm.get_language("python").unwrap();
+    let tb = sourceview::Buffer::new_with_language(&python);
+    let tv = sourceview::View::new_with_buffer(&tb);
+    setup_textview(&tv);
+
+    let (def_changed_tx, def_changed_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+    let work = Arc::new(Work::new());
+
     let work1 = Arc::clone(&work);
     thread::spawn(move || {
-        let mut st = PyState {
-            defs: HashMap::new(),
-            names: BiMap::new(),
-            dependents: HashMap::new(),
-            prim_dependents: HashMap::new(),
-            prev: HashMap::new(),
-            len: HashMap::new(),
-            branches: HashMap::new()
-        };
+        let mut st = PyState::new();
         let gil = Python::acquire_gil();
         let py = gil.python();
         let ast = PyModule::import(py, "ast").unwrap();
-        let ptrs = PyPtrs{
-            ret_ty: (ast.getattr("Return").unwrap()).as_ptr(),
-            asn_ty: (ast.getattr("Assign").unwrap()).as_ptr(),
-            bin_ty: (ast.getattr("BinOp").unwrap()).as_ptr(),
-            call_ty: (ast.getattr("Call").unwrap()).as_ptr(),
-            name_ty: (ast.getattr("Name").unwrap()).as_ptr(),
-            const_ty: (ast.getattr("Constant").unwrap()).as_ptr()
-        };
+        let ptrs = PyPtrs::new(ast);
         loop {
             {
-                if work1.die.load(Ordering::Relaxed) {
-                    break
-                }
+                if work1.die.load(Ordering::Relaxed) { break }
                 work1.do_work.store(work1.content.lock().unwrap().is_some(), Ordering::Relaxed);
             }
             thread::sleep(Duration::from_millis(500));
@@ -319,74 +297,33 @@ fn spawn_parser(receiver: mpsc::Receiver<(GString, i32)>, sender: glib::Sender<P
                     Ok(val) => {
                         for v in val {
                             for vv in v{
-                                sender.send(vv).unwrap();
-                            }
-                        }
-                    },
-                    Err(e) => e.print(py)
-                }
-            }
-        }
-    });
-    let work2 = Arc::clone(&work);
-    thread::spawn(move || {
-        loop {
-            let a = receiver.recv();
-            match a {
-                Ok(o) => {
-                    *(work2.content.lock().unwrap()) = Some(o);
-                    work2.do_work.store(false, Ordering::Relaxed);
-                },
-                _ => work2.die.store(true, Ordering::Relaxed)
-            }
-
-        }
-    });
-}
-
-fn build_ui(app: &Application) {
-    let window = ApplicationWindow::new(app);
-    window.set_title("Sting");
-    window.set_default_size(350, 70);
-    let lm = sourceview::LanguageManager::get_default().unwrap();
-    let python = lm.get_language("python").unwrap();
-    let tb = sourceview::Buffer::new_with_language(&python);
-    let tv = sourceview::View::new_with_buffer(&tb);
-    tv.set_auto_indent(true);
-    tv.set_indent_on_tab(true);
-    tv.set_smart_backspace(true);
-    tv.set_indent_width(2);
-
-    let (new_input_tx, new_input_rx) = mpsc::sync_channel(1);
-    let new_input_tx_cell = cell::RefCell::new(new_input_tx);
-
-    let (def_changed_tx, def_changed_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    spawn_parser(new_input_rx, def_changed_tx);
+                                def_changed_tx.send(vv).unwrap(); }}},
+                    Err(e) => e.print(py) }}}});
 
     let tv2 = tv.clone();
     def_changed_rx.attach(None, move |nm| {
-        // eprintln!("GOT {:?}", nm);
         add_history_header(&tv2, nm);
         glib::Continue(true)
     });
 
+    let work2 = Arc::clone(&work);
     tb.connect_highlight_updated(move |tba, start, end| {
         let mut itrb = start.clone();
         if !tba.iter_has_context_class(&itrb, "funcdef") {
             if tba.iter_backward_to_context_class_toggle(&mut itrb, "funcdef") {
                 if !tba.iter_has_context_class(&itrb, "funcdef") {
-                    tba.iter_backward_to_context_class_toggle(&mut itrb, "funcdef");
-                }
-            }
-        }
+                    tba.iter_backward_to_context_class_toggle(&mut itrb, "funcdef"); } } }
         let mut itrf = end.clone();
         tba.iter_forward_to_context_class_toggle(&mut itrf, "funcdef");
         match tba.get_text(&itrb, &itrf, true) {
-            Some(s) => new_input_tx_cell.borrow_mut().send((s, itrb.get_line())).unwrap(),
-            None => ()
-        }
-    });
+            Some(s) => {
+                *(work2.content.lock().unwrap()) = Some((s, itrb.get_line()));
+                work2.do_work.store(false, Ordering::Relaxed);}
+            None => ()}});
 
+   app.connect_shutdown(move |_| {
+        work.die.store(true, Ordering::Relaxed);
+   });
    window.add(&tv);
    window.show_all();
 }
@@ -408,5 +345,4 @@ fn main() {
     });
 
     application.connect_activate(build_ui);
-    application.run(&[]);
-}
+    application.run(&[]);}
